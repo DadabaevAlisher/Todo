18235331ce31ffef51f0ccf9e05662358c49532c
Alisher <dadabaev027@gmail.com>
2025-08-06 17:19:33 +0500

    Add index and add view post

--- main/views.py
+++ main/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import Todo
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 
+class IndexView(LoginRequiredMixin, View):
+    def get(self, request):
+        todos = Todo.objects.filter(user=request.user)
+        return render(request, 'index.html', {'todos': todos})  
+    
+    def post(self, request):
+        body = request.POST.get('body')
+        if body:
+            Todo.objects.create(user=request.user, body=body)
+        return redirect('index')
