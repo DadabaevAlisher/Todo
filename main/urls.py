f00ad69364be7c38cc9581de7f906a22496b28dd
Alisher <dadabaev027@gmail.com>
2025-08-06 18:05:19 +0500

    inital commit

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, TodoActivateView, logout_view
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('<int:todo_id>/<str:action>/action/', TodoActivateView.as_view(), name='action'),
+    path('logout/', logout_view, name='logout'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render, redirect
 from django.views import View
 from .models import Todo
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import logout
 # Create your views here.
 
 
 class IndexView(LoginRequiredMixin, View):
     def get(self, request):
-        todos = Todo.objects.filter(user=request.user)
+        todos = Todo.objects.filter(user=request.user).order_by('-datetime')
         return render(request, 'index.html', {'todos': todos})  
     
     def post(self, request):
@@ -15,3 +16,22 @@ class IndexView(LoginRequiredMixin, View):
         if body:
             Todo.objects.create(user=request.user, body=body)
         return redirect('index')
+    
+
+class TodoActivateView(LoginRequiredMixin, View):
+    def post(self, request, todo_id, action):
+        todo = Todo.objects.get(id=todo_id, user=request.user)
+        if todo:
+            if action == 'done':
+                todo.done = True
+                todo.save()
+            elif action == 'delete':
+                todo.delete()
+
+            return redirect('index')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+        
